208345389dd6646f37e8875c5c871c23224ef6d9
Temzasse <teemu.taskula@taitounited.fi>
2018-08-30 09:04:34 +0300

    feat: update API to remove manually defining types and to use object arg.
    BREAKING CHANGE: reworked lib api.

---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+export const capitalize = word => word.charAt(0).toUpperCase() + word.slice(1);
+
+export const isFunction = f => f && {}.toString.call(f) === '[object Function]';
+
+export const camelCasedAction = action =>
+  action
+    .toLowerCase()
+    .split('_')
+    .reduce((acc, x, i) => (i === 0 ? acc + x : acc + capitalize(x)), '');
+
+export const validateInject = injected => {
+  if (!Array.isArray(injected)) {
+    throw Error('Inject should be an array of dependency names');
+  }
+
+  const hasNonStringInjects =
+    injected.map(dep => typeof dep !== 'string').filter(Boolean).length > 0;
+
+  if (hasNonStringInjects) {
+    throw Error('Injected dependency names should be strings');
+  }
+};
